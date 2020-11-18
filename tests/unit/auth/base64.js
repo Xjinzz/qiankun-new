c26b86c9e6a1502437a14993cf5643996d7cfe56
jinzz <jinzhenzong@lenovo.com>
2020-11-18 14:18:02 +0800

    FIX: 删除测试文件

--- tests/unit/auth/base64.js
+++
@@ -1,4 +0,0 @@
-var Base64 = require('js-base64').Base64
-const str = '1234'
-const result = Base64.encode(str)
-console.log(result)
--- tests/unit/uuid.js
+++
@@ -1,4 +0,0 @@
-const uuidv4 = require('uuid/v4')
-const str = uuidv4().replace(/-/g, '')
-console.log(str)
-console.log(str.length)
